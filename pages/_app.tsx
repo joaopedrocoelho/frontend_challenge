760d348509b4af0b5eb3c003d95aeae330bf54c3
Pedro Coelho <joaopedrocoelho2@gmail.com>
2021-11-19 12:40:10 +0800

    reduced network requests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { useEffect, useState } from 'react';
+import dataContext from '../context/dataContext';
+import { DataObject } from "../context/datatype";
+import { useRouter } from "next/router";
+import useFetch from "../hooks/useFetch";
+
 import type { AppProps } from 'next/app'
+import { useData } from '../hooks/useData';
+import pageContext from '../context/pageNumber';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const dataHook = useData();
+  const [pagesLoaded, nextPage] = useState(1)
+
+
+  return(
+    <pageContext.Provider value={{pagesLoaded,nextPage}}>
+    <dataContext.Provider value={dataHook}>
+      <Component {...pageProps} />
+    </dataContext.Provider>
+    </pageContext.Provider>
+  ) 
 }
 
 export default MyApp
