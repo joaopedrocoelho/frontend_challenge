821f98292c8428e17895c8dae48fbee6521642c2
Pedro Coelho <joaopedrocoelho2@gmail.com>
2021-11-19 23:43:13 +0800

    fix fetchData

--- components/LoadMore/LoadMore.tsx
+++ components/LoadMore/LoadMore.tsx
@@ -9,34 +9,28 @@ import pageContext from "../../context/pageNumber";
 const LoadMore = () => {
   const { pathname, asPath, query } = useRouter();
   const { data, setData, isLoading, setLoading } = useContext(dataContext);
-  const [ pagesLoaded, setPagesLoaded ] = useState(1) ;
-
-  const fetchData = (page:number) => {
-    setLoading(true);
-    useFetch(page).then((res) => {
-      console.log('data', data)
-      console.log('test',[...data,...res.results])
-      setData([...data, ...res.results]);
-      console.log("res", res);
-      setLoading(false);
-    });
-  }
-  
+  const [pagesLoaded, setPagesLoaded] = useState(1);
 
   return (
-    
-      <button onClick={() => {
-          setPagesLoaded(pagesLoaded+1)
-          fetchData(pagesLoaded+1);
+    <button
+      onClick={() => {
+        setPagesLoaded(pagesLoaded + 1);
+        setLoading(true);
+        useFetch(pagesLoaded + 1).then((res) => {
+          console.log("data", data);
+          console.log("test", [...data, ...res.results]);
+          setData([...data, ...res.results]);
+          console.log("res", res);
+          setLoading(false);
+        });
       }}
-        className={`mt-6  rounded-lg flex flex-row flex-nowrap 
+      className={`mt-6  rounded-lg flex flex-row flex-nowrap 
         place-items-center p-4 bg-green-100 w-max
         cursor-pointer`}
-      >
-        <LoadingIcon className={`w-8 ${isLoading && `animate-spin`}`} />
-        <span className={`pl-2`}>{isLoading ? 'Loading': 'Load More'}</span>
-      </button>
-   
+    >
+      <LoadingIcon className={`w-8 ${isLoading && `animate-spin`}`} />
+      <span className={`pl-2`}>{isLoading ? "Loading" : "Load More"}</span>
+    </button>
   );
 };
 
